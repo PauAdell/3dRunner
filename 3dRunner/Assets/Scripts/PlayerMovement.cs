36a9adbb69d5042d460184b5f114b3111a48280c
Ivan Gonzalez Reguera <ivangonregue01@gmail.com>
2023-05-03 15:36:50 +0200

    arreglado salto
    arreglado salto

--- 3dRunner/Assets/Scripts/PlayerMovement.cs
+++ 3dRunner/Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,7 @@ public class PlayerMovement : MonoBehaviour
     public bool giro;
     public float jumpForce = 50.0f;
     public float speed = 9.0f;
-    public int jump;
+    public int jump = 0;
     private Rigidbody playerRb;
 
     // Start is called before the first frame update
@@ -40,8 +40,8 @@ public class PlayerMovement : MonoBehaviour
             playerRb.AddForce(new Vector3(0, 0.5f, 0) * jumpForce, ForceMode.Impulse);
             ++jump;
         }
-        if (transform.position.y < 5.1f) jump = 0; //cambiar bien paushi
-        print(transform.position.y);
+        print(jump);
+        //print(transform.position.y);
         transform.Translate(0, 0, speed * Time.deltaTime);
         //myCharacterController.Move(transform.forward * speed * Time.deltaTime);
     }
