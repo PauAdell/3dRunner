a97cd3cb9d5a251313587d4d32222e657d4365d1
Ivan Gonzalez Reguera <ivangonregue01@gmail.com>
2023-05-14 16:22:15 +0200

    Animacion salto y caida
    Animacion salto y caida

--- 3dRunner/Assets/Scripts/PlayerMovement.cs
+++ 3dRunner/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
+    public Animator myAnim;
+    public bool is_grounded;
     public int tile;
     private bool action;
+    private bool jump1;
+    private int time_to_jump;
     public bool giro;
     public float jumpForce = 50.0f;
     public float speed = 9.0f;
@@ -17,6 +21,8 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        myAnim = GetComponent<Animator>();
+        jump1 = false;
     }
 
     // Update is called once per frame
@@ -24,7 +30,6 @@ public class PlayerMovement : MonoBehaviour
      {
         action = Input.GetKeyDown(KeyCode.Space);
         //print(speed);
-        if (action) print("action");
         if (tile == 1 && action && !giro)
         {
             transform.Rotate(new Vector3(0f, 90f, 0f));
@@ -36,11 +41,18 @@ public class PlayerMovement : MonoBehaviour
             transform.Rotate(new Vector3(0f, -90f, 0f));
             giro = true;
         }
-        else if (action && jump < 2) {
+        else if (action && jump < 2 && !jump1) {
+            myAnim.StopPlayback();
+            myAnim.Play("Running jump");
             playerRb.AddForce(new Vector3(0, 0.5f, 0) * jumpForce, ForceMode.Impulse);
+            time_to_jump = 100;
             ++jump;
+            jump1 = true;
+            is_grounded = false;
         }
-        print(jump);
+        if (jump1) --time_to_jump;
+        if (time_to_jump == 0) jump1 = false;
+        if (is_grounded) myAnim.Play("running");
         //print(transform.position.y);
         transform.Translate(0, 0, speed * Time.deltaTime);
         //myCharacterController.Move(transform.forward * speed * Time.deltaTime);
--- 3dRunner/Assets/Scripts/TileType.cs
+++ 3dRunner/Assets/Scripts/TileType.cs
@@ -13,21 +13,19 @@ public class TileType : MonoBehaviour
         switch (other.gameObject.tag)
         {
             case "RightTile": playerMovement.tile = 1;
-                print("entra1");
                 break;
             case "LeftTile": playerMovement.tile = 2;
-                print("entra2");
                 break;
             case "BasicTile": playerMovement.tile = 3;
-                print("entra3");
                 break;
             case "SlowTile": playerMovement.speed -= 1;
-                print("entra4");
                 break;
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
             playerMovement.jump = 0;
+            playerMovement.is_grounded = true;
+         
     }
 }
