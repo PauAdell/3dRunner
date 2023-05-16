8788e987e3273d4128c2d8fb44e12dd315409500
Ivan Gonzalez Reguera <ivangonregue01@gmail.com>
2023-05-16 13:18:13 +0200

    djasda
    aaa

---
+++ 3dRunner/Assets/Scripts/Management.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Management : MonoBehaviour
+{
+    private int monedas;
+    private int top_score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        monedas = top_score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "moneda")  monedas += 1;
+    }
+
+    public void EndGame() {
+        if (monedas > top_score) top_score = monedas;
+        monedas = 0;
+    }
+}
--- 3dRunner/Assets/Scripts/PlayerMovement.cs
+++ 3dRunner/Assets/Scripts/PlayerMovement.cs
@@ -9,20 +9,18 @@ public class PlayerMovement : MonoBehaviour
     public bool is_grounded;
     public int tile;
     private bool action;
-    private bool jump1;
-    private int time_to_jump;
     public bool giro;
     public float jumpForce = 50.0f;
     public float speed = 9.0f;
     public int jump = 0;
     private Rigidbody playerRb;
+    private int in_anim;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         myAnim = GetComponent<Animator>();
-        jump1 = false;
     }
 
     // Update is called once per frame
@@ -30,29 +28,30 @@ public class PlayerMovement : MonoBehaviour
      {
         action = Input.GetKeyDown(KeyCode.Space);
         //print(speed);
-        if (tile == 1 && action && !giro)
+        if (tile == 1 && action && !giro && is_grounded)
         {
             transform.Rotate(new Vector3(0f, 90f, 0f));
             giro = true;
+            myAnim.StopPlayback();
+            myAnim.Play("Right turning");
+            in_anim = 60;
 
         }
-        else if (tile == 2 && action && !giro)
+        else if (tile == 2 && action && !giro && is_grounded)
         {
             transform.Rotate(new Vector3(0f, -90f, 0f));
             giro = true;
         }
-        else if (action && jump < 2 && !jump1) {
+        else if (action && jump < 2 && in_anim == 0) {
             myAnim.StopPlayback();
             myAnim.Play("Running jump");
             playerRb.AddForce(new Vector3(0, 0.5f, 0) * jumpForce, ForceMode.Impulse);
-            time_to_jump = 100;
+            in_anim = 100;
             ++jump;
-            jump1 = true;
             is_grounded = false;
         }
-        if (jump1) --time_to_jump;
-        if (time_to_jump == 0) jump1 = false;
-        if (is_grounded) myAnim.Play("running");
+        if (in_anim > 0) --in_anim;
+        if (is_grounded && in_anim == 0) myAnim.Play("running");
         //print(transform.position.y);
         transform.Translate(0, 0, speed * Time.deltaTime);
         //myCharacterController.Move(transform.forward * speed * Time.deltaTime);
