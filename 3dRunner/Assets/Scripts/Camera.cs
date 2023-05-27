c012a6f92595ade3c1e904116544a94fff010ab5
Ivan Gonzalez Reguera <101863690+ImKvothe@users.noreply.github.com>
2023-05-27 17:43:09 +0200

    Revert "Ivan"

--- 3dRunner/Assets/Scripts/Camera.cs
+++ 3dRunner/Assets/Scripts/Camera.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     public Transform target;
-    public float distance = 35.0f;
+    public float distance = 30.0f;
     public float heightOffset = 30.0f;
     public float cameraDelay = 0.02f;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 forward = new Vector3(0f, 0f, 1f);
-        Vector3 followPos = target.position + forward * distance;
+        Vector3 followPos = target.position + target.forward * distance;
+
         followPos.y += heightOffset;
         followPos.x += distance;
-        transform.position += (followPos - transform.position);
+        transform.position += (followPos - transform.position) * cameraDelay;
 
         transform.LookAt(target.transform);
     }
--- 3dRunner/Assets/Scripts/TileType.cs
+++ 3dRunner/Assets/Scripts/TileType.cs
@@ -5,67 +5,29 @@ using UnityEngine;
 public class TileType : MonoBehaviour
 {
     public PlayerMovement playerMovement;
-    private float initial_speed;
-
-    void Start()
-    {
-        initial_speed = playerMovement.speed;
-    }
     private void OnTriggerEnter(Collider other)
     {
-        playerMovement.goalPosition = other.bounds.center;
         playerMovement.tile = 3;
         playerMovement.giro = false;
-        playerMovement.speed = 5.0f;
+        playerMovement.speed = 4.0f;
         switch (other.gameObject.tag)
         {
             case "RightTile": playerMovement.tile = 1;
-                playerMovement.target = playerMovement.transform.position.z + 1;
-                if (playerMovement.god_mode)
-                {
-                    playerMovement.girando = true;
-                    playerMovement.giro = true;
-                    playerMovement.aprox = true;
-                    playerMovement.current = transform.position.z;
-                }
-                    break;
+                print("entra1");
+                break;
             case "LeftTile": playerMovement.tile = 2;
-                playerMovement.target = playerMovement.transform.position.x + 1;
-                if (playerMovement.god_mode)
-                {
-                    playerMovement.girando = true;
-                    playerMovement.giro = true;
-                    playerMovement.aprox = true;
-                    playerMovement.current = transform.position.z;
-                }
+                print("entra2");
                 break;
             case "BasicTile": playerMovement.tile = 3;
+                print("entra3");
                 break;
-            case "SlowTile": if (!playerMovement.god_mode) playerMovement.speed -= 1;
-                break;
-            case "Trap": if (!playerMovement.god_mode) playerMovement.muerte = 1;
-                break;
-            case "Trap2": if (!playerMovement.god_mode) playerMovement.muerte = 2;
-                break;
-            case "TileSalto": playerMovement.tile = 3;
-                if (playerMovement.god_mode) playerMovement.auto_salto = true;
-                break;
-            case "TileSalto2":
-                playerMovement.tile = 4;
-                if (playerMovement.god_mode) playerMovement.auto_salto = true;
+            case "SlowTile": playerMovement.speed -= 1;
+                print("entra4");
                 break;
         }
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("SlowTile")) if (playerMovement.speed == initial_speed && !playerMovement.god_mode) playerMovement.speed -= 1;
     }
     private void OnCollisionEnter(Collision collision)
     {
             playerMovement.jump = 0;
-            playerMovement.is_grounded = true;
-         
     }
-
 }
