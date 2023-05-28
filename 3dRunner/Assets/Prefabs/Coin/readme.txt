24f8e7c8e773b36bb1448b87a9252cd7b3fbed06
Ivan Gonzalez Reguera <ivangonregue01@gmail.com>
2023-05-28 18:14:22 +0200

    moneda, meteorite movement

---
+++ 3dRunner/Assets/Prefabs/Coin/readme.txt
@@ -0,0 +1,11 @@
+	~INFO~
+- Model: Coin (Yellow, Red, Blue)
+- Game: Mario + Rabbids Kingdom Battle
+- Ripped by: DJ_Fox11
+- Uploaded to: The Models Resource
+
+	~CREDIT~
+- Credit to me (DJ_Fox11) is optional, but credit to The Models Resource is required.
+
+	~OTHER INFO~
+- If this model is used for online purposes, please include this file.
--- 3dRunner/Assets/Scripts/CoinMovement.cs
+++ 3dRunner/Assets/Scripts/CoinMovement.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class CoinMovement : MonoBehaviour
 {
     private Vector3 _startPosition;
+    public PlayerMovement playerMovement;
     // Start is called before the first frame update
     void Start()
     {
         _startPosition = transform.position;
+        gameObject.GetComponentInChildren<MeshRenderer>().enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        transform.Rotate(new Vector3(0f, 2.5f, 0f));
+        if (!playerMovement.start) gameObject.GetComponentInChildren<MeshRenderer>().enabled = true;
+        transform.Rotate(new Vector3(0f,  2.5f, 0f));
         transform.position = _startPosition + new Vector3(0.0f, Mathf.Sin(Time.time)/2f, 0.0f);
     }
 }
