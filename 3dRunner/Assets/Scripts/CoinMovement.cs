ddbd17d20fb74cd6d972f26bd078a1485f5c623e
Ivan Gonzalez Reguera <ivangonregue01@gmail.com>
2023-06-03 00:41:14 +0200

    mejora coin

--- 3dRunner/Assets/Scripts/CoinMovement.cs
+++ 3dRunner/Assets/Scripts/CoinMovement.cs
@@ -7,10 +7,12 @@ public class CoinMovement : MonoBehaviour
     private Vector3 _startPosition;
     public PlayerMovement playerMovement;
     public bool vanishing;
+    private Quaternion _startRotation;
     // Start is called before the first frame update
     void Start()
     {
         _startPosition = transform.position;
+        _startRotation = transform.rotation;
         gameObject.GetComponentInChildren<MeshRenderer>().enabled = true;
         vanishing = false;
     }
@@ -24,9 +26,16 @@ public class CoinMovement : MonoBehaviour
             gameObject.GetComponentInChildren<MeshRenderer>().enabled = true;
             vanishing = false;
         }
-        transform.Rotate(new Vector3(0f,  2.5f, 0f));
-        if (!vanishing) transform.position = _startPosition + new Vector3(0.0f, Mathf.Sin(Time.time)/2f, 0.0f);
-        if (vanishing) transform.position = new Vector3(transform.position.x, transform.position.y + 0.05f, transform.position.z);
+        if (!vanishing)
+        {
+            transform.position = _startPosition + new Vector3(0.0f, Mathf.Sin(Time.time) / 2f, 0.0f);
+            transform.Rotate(new Vector3(0f, 2.5f, 0f));
+        }
+        else if (vanishing)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.05f, transform.position.z);
+            transform.rotation = _startRotation;
+        }
         if (transform.position.y >= _startPosition.y + 3) gameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
     }
 
