ddf6517a6b3f1b51e30911b515d6d18c2504d818
Ivan Gonzalez Reguera <ivangonregue01@gmail.com>
2023-06-03 00:27:26 +0200

    moneda animacion

--- 3dRunner/Assets/Scripts/CoinMovement.cs
+++ 3dRunner/Assets/Scripts/CoinMovement.cs
@@ -6,11 +6,13 @@ public class CoinMovement : MonoBehaviour
 {
     private Vector3 _startPosition;
     public PlayerMovement playerMovement;
+    public bool vanishing;
     // Start is called before the first frame update
     void Start()
     {
         _startPosition = transform.position;
         gameObject.GetComponentInChildren<MeshRenderer>().enabled = true;
+        vanishing = false;
     }
 
     // Update is called once per frame
@@ -20,9 +22,17 @@ public class CoinMovement : MonoBehaviour
         if (!playerMovement.start)
         {
             gameObject.GetComponentInChildren<MeshRenderer>().enabled = true;
+            vanishing = false;
         }
         transform.Rotate(new Vector3(0f,  2.5f, 0f));
-        transform.position = _startPosition + new Vector3(0.0f, Mathf.Sin(Time.time)/2f, 0.0f);
+        if (!vanishing) transform.position = _startPosition + new Vector3(0.0f, Mathf.Sin(Time.time)/2f, 0.0f);
+        if (vanishing) transform.position = new Vector3(transform.position.x, transform.position.y + 0.05f, transform.position.z);
+        if (transform.position.y >= _startPosition.y + 3) gameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
+    }
+
+    public void setVanishing()
+    {
+        vanishing = true;
     }
 
 }
